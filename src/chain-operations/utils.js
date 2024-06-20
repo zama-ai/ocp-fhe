ac5703eeac3fbf5277049e4d1a6721e71e22ba51
Adam Momen <adammuman81@gmail.com>
2024-06-20 22:59:37 +0200

    default IS_DEV variable to true

--- src/chain-operations/utils.js
+++ src/chain-operations/utils.js
@@ -1,3 +1,3 @@
-const IS_DEV = process.env.NODE_ENV === "development";
+const IS_DEV = process.env.NODE_ENV === "development" || true;
 export const getJoiErrorMessage = (error) => (error.details.length > 0 ? error.details[0].message : "Invalid request");
 export const API_URL = `https://api.series${IS_DEV ? "-dev" : ""}.fairmint.co/admin`;
