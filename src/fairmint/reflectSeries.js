a0c4f8258dabefeb5c90c86dece9625da6f2c22b
Victor Mimo <victormimoc@gmail.com>
2024-06-21 10:21:30 +0200

    helper

---
+++ src/fairmint/reflectSeries.js
@@ -0,0 +1,16 @@
+import { API_URL } from "./utils.js";
+
+export const reflectSeries = async ({ issuerId, custom_id, series_name, stock_class_id, stock_plan_id }) => {
+    console.log("Reflecting Series in fairmint...");
+
+    const reflectSeriesResponse = await axios.post(`${API_URL}/ocp/reflectSeries?portalId=${issuerId}`, {
+        series_id: custom_id,
+        series_name,
+        stock_class_id,
+        stock_plan_id,
+    });
+
+    console.log("Successfully reflected Series into Fairmint");
+
+    return reflectSeriesResponse.data.data;
+};
