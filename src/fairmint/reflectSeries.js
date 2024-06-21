a9b6deae87ef988b5aeb5474184388052da6deec
Adam Momen <adammuman81@gmail.com>
2024-06-21 12:19:30 +0200

    fix typo

--- src/fairmint/reflectSeries.js
+++ src/fairmint/reflectSeries.js
@@ -1,10 +1,12 @@
 import { API_URL } from "../chain-operations/utils";
+import axios from "axios";
 
-export const reflectSeries = async ({ issuerId, custom_id, series_name, stock_class_id, stock_plan_id }) => {
+export const reflectSeries = async ({ issuerId, series_id, series_name, stock_class_id, stock_plan_id }) => {
     console.log("Reflecting Series in fairmint...");
+    console.log({ issuerId, series_id, series_name, stock_class_id, stock_plan_id });
 
     const reflectSeriesResponse = await axios.post(`${API_URL}/ocp/reflectSeries?portalId=${issuerId}`, {
-        series_id: custom_id,
+        series_id,
         series_name,
         stock_class_id,
         stock_plan_id,
