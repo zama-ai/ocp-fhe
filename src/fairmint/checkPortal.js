e4fc008b0f38adc1326d0e7c3c7a3d224bffd8d9
Adam Momen <adammuman81@gmail.com>
2024-06-27 13:54:45 +0200

    Add `stakeholder` checker on fairmint DB

---
+++ src/fairmint/checkPortal.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import get from "lodash/get";
+import { API_URL } from "../chain-operations/utils";
+
+export const checkPortal = async ({ portalId }) => {
+    const webHookUrl = `${API_URL}/ocp/checkPortal?portalId=${portalId}`;
+    console.log({ portalId });
+
+    try {
+        console.log("Checking portal status in Fairmint...");
+        const resp = await axios.get(webHookUrl);
+
+        return resp.data;
+    } catch (error) {
+        if (error.response) {
+            const formattedError = {
+                status: error.response.status,
+                endpoint: webHookUrl,
+                data: get(error, "response.data"),
+            };
+            throw Error(`Error checking portal status in Fairmint: ${JSON.stringify(formattedError, null, 2)}`);
+        } else {
+            throw Error(`Error checking portal status in Fairmint: ${error.message}`);
+        }
+    }
+};
