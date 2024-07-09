3509991894dd984f889effab4617d2c115c6ec3d
Adam Momen <adammuman81@gmail.com>
2024-07-09 22:15:56 +0800

    Query total amount of stock plan

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { find, countDocuments } from "../db/operations/atomic";
 import Stakeholder from "../db/objects/Stakeholder.js";
+import StockPlan from "../db/objects/StockPlan.js";
 import StockIssuance from "../db/objects/transactions/issuance/StockIssuance.js";
 import ConvertibleIssuance from "../db/objects/transactions/issuance/ConvertibleIssuance.js";
 import get from "lodash/get";
@@ -20,10 +21,16 @@ dashboard.get("/", async (req, res) => {
     const convertibleIssuances = await find(ConvertibleIssuance, { issuer: issuerId });
     const totalConvertibleAmount = convertibleIssuances.reduce((acc, issuance) => acc + Number(issuance.investment_amount.amount), 0);
     const totalRaised = totalStockAmount + totalConvertibleAmount;
+    const stockPlans = await find(StockPlan, { issuer: issuerId })// .reduce((acc, plan) => acc + Number(get(plan, "initial_shares_reserved")), 0);
+    const stockPlanAmount = stockPlans.reduce((acc, plan) => acc + Number(get(plan, "initial_shares_reserved")), 0);
+
+    console.log({stockPlanAmount})
+    console.log({stockPlanAmount})
 
     res.status(200).send({
         numOfStakeholders,
         totalRaised,
+        stockPlanAmount
     });
 });
 
