1e4cd462d91bdeacf5ded53b2404c7dccecdc1cd
Adam Momen <adammuman81@gmail.com>
2024-08-22 22:40:38 +0800

    get provider once

--- src/chain-operations/getContractInstances.js
+++ src/chain-operations/getContractInstances.js
@@ -6,14 +6,13 @@ import getProvider from "./getProvider.js";
 
 setupEnv();
 
+const provider = getProvider();
 export const getContractInstance = (address) => {
     const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY;
-
-    const provider = getProvider();
 
     const wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider);
     const contract = new ethers.Contract(address, CAP_TABLE.abi, wallet);
     const libraries = getTXLibContracts(contract.target, wallet);
 
     return { contract, provider, libraries };
-}
+};
