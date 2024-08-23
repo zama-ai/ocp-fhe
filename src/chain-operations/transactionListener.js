581ac069ec3b27f2cfcdb8a588a55e1c4a66a1f3
Adam Momen <adammuman81@gmail.com>
2024-08-23 14:29:16 +0800

    Add processing locking and pop / push queue upon successful processing

--- src/chain-operations/transactionListener.js
+++ src/chain-operations/transactionListener.js
@@ -27,10 +27,10 @@ import {
 import { readFairmintDataById } from "../db/operations/read.js";
 import { API_URL } from "../fairmint/config.js";
 import axios from "axios";
+import get from "lodash/get.js";
 
 const abiCoder = new AbiCoder();
-const eventQueue = [];
-const issuerEventFired = new Set();
+
 const txMapper = {
     0: ["INVALID"],
     1: ["ISSUER_AUTHORIZED_SHARES_ADJUSTMENT", IssuerAuthorizedSharesAdjustment],
@@ -44,34 +44,62 @@ const txMapper = {
     9: ["STOCK_TRANSFER", StockTransfer],
 };
 
+const contractQueues = new Map();
+
 async function startOnchainListeners(contract, provider, issuerId, libraries) {
+    if (!contractQueues.has(issuerId)) {
+        contractQueues.set(issuerId, []);
+    }
+    let isProcessing = false;
+    let issuerEventReceived = false;
+
     console.log("🌐 | Initiating on-chain event listeners for issuer", issuerId, "at address", contract.target);
+
+    const processEventQueue = async () => {
+        if (isProcessing) return;
+        const eventQueue = contractQueues.get(issuerId);
+        eventQueue.sort((a, b) => a.timestamp - b.timestamp);
+        let event;
+        try {
+            while (eventQueue.length > 0) {
+                event = eventQueue[0]; // Peek the queue
+                await processEvent(event);
+                eventQueue.shift(); // Pop the queue after processing
+            }
+        } catch (error) {
+            console.error(`Error processing event of type ${get(event, "type", event)}:`, error);
+        } finally {
+            isProcessing = false;
+        }
+    };
+
+    const queueAndProcessEvent = async (event) => {
+        const eventQueue = contractQueues.get(issuerId);
+        eventQueue.push(event);
+        await processEventQueue();
+    };
 
     libraries.txHelper.on("TxCreated", async (_, txTypeIdx, txData, event) => {
         const [type, structType] = txMapper[txTypeIdx];
         const decodedData = abiCoder.decode([structType], txData);
         const { timestamp } = await provider.getBlock(event.blockNumber);
-        eventQueue.push({ type, data: decodedData[0], issuerId, timestamp });
+        await queueAndProcessEvent({ type, data: decodedData[0], issuerId, timestamp });
     });
 
-    contract.on("StakeholderCreated", async (id, _) => {
+    contract.on("StakeholderCreated", async (id) => {
         console.log("Create Stakeholder event");
-        eventQueue.push({ type: "STAKEHOLDER_CREATED", data: id });
+        await queueAndProcessEvent({ type: "STAKEHOLDER_CREATED", data: id, issuerId });
     });
 
-    contract.on("StockClassCreated", async (id, _) => {
-        eventQueue.push({ type: "STOCK_CLASS_CREATED", data: id });
+    contract.on("StockClassCreated", async (id) => {
+        await queueAndProcessEvent({ type: "STOCK_CLASS_CREATED", data: id, issuerId });
     });
 
+    // Issuer Initialization: This is the first time we're processing the issuer.
     const issuerCreatedFilter = contract.filters.IssuerCreated;
     const issuerEvents = await contract.queryFilter(issuerCreatedFilter);
 
-    console.log({
-        issuerEvents,
-        issuerEventFired,
-    });
-
-    if (issuerEvents.length > 0 && !issuerEventFired.has(issuerEvents[0].args[0])) {
+    if (issuerEvents.length > 0 && !issuerEventReceived) {
         const id = issuerEvents[0].args[0];
         console.log("IssuerCreated Event Emitted!", id);
         console.log("New issuer was deployed", { issuerId: id });
@@ -87,58 +115,53 @@ async function startOnchainListeners(contract, provider, issuerId, libraries) {
         }
 
         await verifyIssuerAndSeed(contract, id);
-        issuerEventFired.add(issuerEvents[0].args[0]);
+        issuerEventReceived = true;
     }
-
-    setInterval(processEventQueue, 5000); // Process every 5 seconds
 }
 
-async function processEventQueue() {
-    const sortedEventQueue = eventQueue.sort((a, b) => a.timestamp - b.timestamp);
-    while (sortedEventQueue.length > 0) {
-        const event = eventQueue[0];
-        try {
-            switch (event.type) {
-                case "STAKEHOLDER_CREATED":
-                    await handleStakeholder(event.data);
-                    break;
-                case "STOCK_CLASS_CREATED":
-                    await handleStockClass(event.data);
-                    break;
-                case "ISSUER_AUTHORIZED_SHARES_ADJUSTMENT":
-                    await handleIssuerAuthorizedSharesAdjusted(event.data, event.issuerId, event.timestamp);
-                    break;
-                case "STOCK_CLASS_AUTHORIZED_SHARES_ADJUSTMENT":
-                    await handleStockClassAuthorizedSharesAdjusted(event.data, event.issuerId, event.timestamp);
-                    break;
-                case "STOCK_ACCEPTANCE":
-                    await handleStockAcceptance(event.data, event.issuerId, event.timestamp);
-                    break;
-                case "STOCK_CANCELLATION":
-                    await handleStockCancellation(event.data, event.issuerId, event.timestamp);
-                    break;
-                case "STOCK_ISSUANCE":
-                    await handleStockIssuance(event.data, event.issuerId, event.timestamp);
-                    break;
-                case "STOCK_REISSUANCE":
-                    await handleStockReissuance(event.data, event.issuerId, event.timestamp);
-                    break;
-                case "STOCK_REPURCHASE":
-                    await handleStockRepurchase(event.data, event.issuerId, event.timestamp);
-                    break;
-                case "STOCK_RETRACTION":
-                    await handleStockRetraction(event.data, event.issuerId, event.timestamp);
-                    break;
-                case "STOCK_TRANSFER":
-                    await handleStockTransfer(event.data, event.issuerId, event.timestamp);
-                    break;
-                case "INVALID":
-                    throw new Error("Invalid transaction type");
-            }
-        } catch (error) {
-            console.error(`Error processing event of type ${event.type}:`, error);
-        }
-        sortedEventQueue.shift();
+async function processEvent(event) {
+    if (!event) {
+        console.error("WARN:processEvent: Event inside is undefined");
+        return;
+    }
+    switch (event.type) {
+        case "STAKEHOLDER_CREATED":
+            await handleStakeholder(event.data);
+            break;
+        case "STOCK_CLASS_CREATED":
+            await handleStockClass(event.data);
+            break;
+        case "ISSUER_AUTHORIZED_SHARES_ADJUSTMENT":
+            await handleIssuerAuthorizedSharesAdjusted(event.data, event.issuerId, event.timestamp);
+            break;
+        case "STOCK_CLASS_AUTHORIZED_SHARES_ADJUSTMENT":
+            await handleStockClassAuthorizedSharesAdjusted(event.data, event.issuerId, event.timestamp);
+            break;
+        case "STOCK_ACCEPTANCE":
+            await handleStockAcceptance(event.data, event.issuerId, event.timestamp);
+            break;
+        case "STOCK_CANCELLATION":
+            await handleStockCancellation(event.data, event.issuerId, event.timestamp);
+            break;
+        case "STOCK_ISSUANCE":
+            await handleStockIssuance(event.data, event.issuerId, event.timestamp);
+            break;
+        case "STOCK_REISSUANCE":
+            await handleStockReissuance(event.data, event.issuerId, event.timestamp);
+            break;
+        case "STOCK_REPURCHASE":
+            await handleStockRepurchase(event.data, event.issuerId, event.timestamp);
+            break;
+        case "STOCK_RETRACTION":
+            await handleStockRetraction(event.data, event.issuerId, event.timestamp);
+            break;
+        case "STOCK_TRANSFER":
+            await handleStockTransfer(event.data, event.issuerId, event.timestamp);
+            break;
+        case "INVALID":
+            throw new Error("Invalid transaction type");
+        default:
+            console.warn(`Unhandled event type: ${event.type}`);
     }
 }
 
