4092939d046d75fe46bec0abca11bb68856829a6
Adam Momen <adammuman81@gmail.com>
2024-08-28 14:49:59 +0800

    fix errors

--- src/routes/stats/helpers.js
+++ src/routes/stats/helpers.js
@@ -1,4 +1,4 @@
-import { find } from "../../db/database.js";
+import { find } from "../../db/operations/atomic.js";
 import StockIssuance from "../../db/objects/transactions/issuance/StockIssuance.js";
 
 export const getStockIssuances = async (issuerId) => {
