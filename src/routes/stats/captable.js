b1968b052ea11b2d0fa856bc16238fa0e0ea0167
Adam Momen <adammuman81@gmail.com>
2024-08-28 14:03:39 +0800

    refactor captable code for more readability

--- src/routes/stats/captable.js
+++ src/routes/stats/captable.js
@@ -7,34 +7,51 @@ const StockClassTypes = {
     COMMON: "COMMON",
     PREFERRED: "PREFERRED",
 };
-const calculateCaptableStats = async (issuerId) => {
-    /*
-                summary:{
-                    stockclass: {
-                        common: {
-                            totalSharesAuthorized: 0,
-                            children: [{
-                                name: "",
-                                total: 0,
-                                initialSharesAuthorized: 0,
-                                outstandingShares: 0,
-                                fullyDilutedShares: 0,
-                                fullyDilutedPercentage: '0.10',
-                                liquidationPreference: 0,
-                                votingPower: 0,
-                                votingPowerPercentage: '0.10',
-                            }]
-                        },
-                        }
-                        preferred: {}
-                    }
-                }
-    */
 
-    const allStockClasses = (await find(Stockclass, { issuer: issuerId })) || [];
-    console.log({ allStockClasses });
-    const commonStockClass = allStockClasses.filter((stockClass) => stockClass.class_type === StockClassTypes.COMMON);
-    const stockIssuances = await find(StockIssuance, { issuer: issuerId });
+const getAllStockClasses = async (issuerId) => {
+    return (await find(Stockclass, { issuer: issuerId })) || [];
+};
+
+const getStockIssuances = async (issuerId) => {
+    return await find(StockIssuance, { issuer: issuerId });
+};
+
+const calculateTotalSharesAuthorized = (stockClasses) => {
+    return stockClasses.reduce((acc, stockClass) => {
+        return acc + Number(stockClass.initial_shares_authorized);
+    }, 0);
+};
+
+const calculateTotalVotingPower = (stockClasses, totalStockIssuanceSharesByStockClass) => {
+    return stockClasses.reduce((acc, stockClass) => {
+        const outstandingShares = totalStockIssuanceSharesByStockClass[stockClass._id] || 0;
+        return acc + Number(stockClass.votes_per_share) * outstandingShares;
+    }, 0);
+};
+
+const calculateStockClassSummary = (stockClasses, totalStockIssuanceSharesByStockClass, totalSharesAuthorized, totalVotingPower) => {
+    return {
+        totalSharesAuthorized: String(totalSharesAuthorized),
+        children: stockClasses.map((stockClass) => {
+            const outstandingShares = get(totalStockIssuanceSharesByStockClass, stockClass._id, null);
+            const votingPower = stockClass.votes_per_share && outstandingShares ? stockClass.votes_per_share * outstandingShares : null;
+            return {
+                name: stockClass.name,
+                initialSharesAuthorized: stockClass.initial_shares_authorized,
+                outstandingShares,
+                fullyDilutedShares: outstandingShares,
+                fullyDilutedPercentage: outstandingShares ? (outstandingShares / totalSharesAuthorized).toFixed(2) : null,
+                liquidationPreference: get(stockClass, "liquidation_preference_multiple"),
+                votingPower,
+                votingPowerPercentage: votingPower && totalVotingPower ? (votingPower / totalVotingPower).toFixed(2) : null,
+            };
+        }),
+    };
+};
+
+const calculateCaptableStats = async (issuerId) => {
+    const allStockClasses = await getAllStockClasses(issuerId);
+    const stockIssuances = await getStockIssuances(issuerId);
 
     const totalStockIssuanceSharesByStockClass = stockIssuances.reduce((acc, issuance) => {
         const stockClassId = issuance.stock_class_id;
@@ -45,64 +62,35 @@ const calculateCaptableStats = async (issuerId) => {
         return acc;
     }, {});
 
-    const commonTotalSharesAuthorized = commonStockClass.reduce((acc, stockClass) => {
-        return acc + Number(stockClass.initial_shares_authorized);
-    }, 0);
+    const commonStockClasses = allStockClasses.filter((stockClass) => stockClass.class_type === StockClassTypes.COMMON);
+    const preferredStockClasses = allStockClasses.filter((stockClass) => stockClass.class_type === StockClassTypes.PREFERRED);
 
-    const preferredStockClass = allStockClasses.filter((stockClass) => stockClass.class_type === StockClassTypes.PREFERRED);
-    const preferredTotalSharesAuthorized = preferredStockClass.reduce((acc, stockClass) => {
-        return acc + Number(stockClass.initial_shares_authorized);
-    }, 0);
+    const commonTotalSharesAuthorized = calculateTotalSharesAuthorized(commonStockClasses);
+    const preferredTotalSharesAuthorized = calculateTotalSharesAuthorized(preferredStockClasses);
 
-    // const preferredStockClass = allStockClasses.filter((stockClass) => stockClass.class_type === "PREFERRED");
-    const commonTotalVotingPower = commonStockClass.reduce((acc, stockClass) => {
-        const outstandingShares = totalStockIssuanceSharesByStockClass[stockClass._id] || 0;
-        return acc + Number(stockClass.votes_per_share) * outstandingShares;
-    }, 0);
-
-    const preferredTotalVotingPower = preferredStockClass.reduce((acc, stockClass) => {
-        const outstandingShares = totalStockIssuanceSharesByStockClass[stockClass._id] || 0;
-        return acc + Number(stockClass.votes_per_share) * outstandingShares;
-    }, 0);
+    const commonTotalVotingPower = calculateTotalVotingPower(commonStockClasses, totalStockIssuanceSharesByStockClass);
+    const preferredTotalVotingPower = calculateTotalVotingPower(preferredStockClasses, totalStockIssuanceSharesByStockClass);
 
     const totalVotingPower = commonTotalVotingPower + preferredTotalVotingPower;
+
     return {
         summary: {
-            common: {
-                totalSharesAuthorized: String(commonTotalSharesAuthorized),
-                children: commonStockClass.map((stockClass) => {
-                    const outstandingShares = get(totalStockIssuanceSharesByStockClass, stockClass._id, null);
-                    const votingPower = stockClass.votes_per_share && outstandingShares ? stockClass.votes_per_share * outstandingShares : null;
-                    return {
-                        name: stockClass.name,
-                        initialSharesAuthorized: stockClass.initial_shares_authorized,
-                        outstandingShares,
-                        fullyDilutedShares: outstandingShares,
-                        fullyDilutedPercentage: outstandingShares ? (outstandingShares / commonTotalSharesAuthorized).toFixed(2) : null,
-                        liquidationPreference: get(stockClass, "liquidation_preference_multiple"),
-                        votingPower,
-                        votingPowerPercentage: votingPower && totalVotingPower ? (votingPower / totalVotingPower).toFixed(2) : null,
-                    };
-                }),
-            },
-            preferred: {
-                totalSharesAuthorized: String(preferredTotalSharesAuthorized),
-                children: preferredStockClass.map((stockClass) => {
-                    const outstandingShares = get(totalStockIssuanceSharesByStockClass, stockClass._id, null);
-                    const votingPower = stockClass.votes_per_share && outstandingShares ? stockClass.votes_per_share * outstandingShares : null;
-                    return {
-                        name: stockClass.name,
-                        initialSharesAuthorized: stockClass.initial_shares_authorized,
-                        outstandingShares,
-                        fullyDilutedShares: outstandingShares,
-                        fullyDilutedPercentage: outstandingShares ? (outstandingShares / preferredTotalSharesAuthorized).toFixed(2) : null,
-                        liquidationPreference: get(stockClass, "liquidation_preference_multiple"),
-                        votingPower,
-                        votingPowerPercentage: votingPower && totalVotingPower ? (votingPower / totalVotingPower).toFixed(2) : null,
-                    };
-                }),
+            stockclasses: {
+                common: calculateStockClassSummary(
+                    commonStockClasses,
+                    totalStockIssuanceSharesByStockClass,
+                    commonTotalSharesAuthorized,
+                    totalVotingPower
+                ),
+                preferred: calculateStockClassSummary(
+                    preferredStockClasses,
+                    totalStockIssuanceSharesByStockClass,
+                    preferredTotalSharesAuthorized,
+                    totalVotingPower
+                ),
             },
         },
     };
 };
+
 export default calculateCaptableStats;
