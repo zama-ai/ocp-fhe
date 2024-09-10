df2c489081d9bcfbe0625e16e20497c6007c1dd0
Victor Mimo <victormimoc@gmail.com>
2024-09-10 18:57:53 -0400

    adding warrants, nonplans and stockPlans

--- src/routes/stats/captable.js
+++ src/routes/stats/captable.js
@@ -1,6 +1,7 @@
 import { find } from "../../db/operations/atomic.js";
 import get from "lodash/get";
 import Stockclass from "../../db/objects/StockClass.js";
+import StockPlan from "../../db/objects/StockPlan.js";
 import { getStockIssuances } from "./helpers.js";
 import WarrantIssuance from "../../db/objects/transactions/issuance/WarrantIssuance.js";
 import EquityCompensationIssuance from "../../db/objects/transactions/issuance/EquityCompensationIssuance.js";
@@ -27,6 +28,10 @@ const getAllStockClasses = async (issuerId) => {
     return (await find(Stockclass, { issuer: issuerId })) || [];
 };
 
+const getAllStockPlans = async (issuerId) => {
+    return (await find(StockPlan, { issuer: issuerId })) || [];
+};
+
 
 const calculateTotalVotingPower = (stockClasses, outstandingSharesByStockClass) => {
     return stockClasses.reduce((acc, stockClass) => {
@@ -46,8 +51,6 @@ const calculateStockClassSummary = (stockClasses, stockIssuances, totalOutstandi
             issuance.stock_class_id === stockClass._id &&
             (excludeIssuanceType ? issuance.issuance_type !== excludeIssuanceType : true)
         );
-
-        console.log('classIssuances', classIssuances);
 
         if (classIssuances.length === 0) return null;
 
@@ -101,44 +104,134 @@ const calculateFounderPreferredSummary = (preferredStockClasses, stockIssuances,
     };
 };
 
+// modularizing the row creation for warrants and non-plan awards
+// for warrants, we need to use the exercise_triggers to get the quantity of shares that the warrant can convert to
+// for non-plan awards, we can just use the quantity
+const createWarrantAndNonPlanAwardsRow = (issuancesByStockClass, stockClasses, totalOutstandingShares, suffix, isWarrant = false) => {
+    return Object.entries(issuancesByStockClass).map(([stockClassId, issuances]) => {
+        const fullyDilutedShares = issuances.reduce((sum, issuance) => {
+            let quantity;
+            if (isWarrant) {
+                quantity = Number(issuance.exercise_triggers?.[0]?.conversion_right?.conversion_mechanism?.converts_to_quantity || 0);
+            } else {
+                quantity = Number(issuance.quantity);
+            }
+            return sum + (isNaN(quantity) ? 0 : quantity);
+        }, 0);
 
+        let name;
+        if (stockClassId === 'general') {
+            name = `General ${suffix}`;
+        } else {
+            const stockClass = stockClasses.find(sc => sc._id === stockClassId);
+            name = stockClass ? `${stockClass.name} ${suffix}` : `Unknown Stock Class ${suffix}`;
+        }
+
+        return {
+            name,
+            fullyDilutedShares,
+            fullyDilutedPercentage: (fullyDilutedShares / totalOutstandingShares * 100).toFixed(2)
+        };
+    });
+}
+
+const createEquityCompensationWithPlanSummaryRows = (equityCompensationByStockPlan, stockPlans, totalOutstandingShares) => {
+    return Object.entries(equityCompensationByStockPlan).map(([stockPlanId, issuances]) => {
+        const fullyDilutedShares = issuances.reduce((sum, issuance) => {
+            const quantity = Number(issuance.quantity);
+            return sum + (isNaN(quantity) ? 0 : quantity);
+        }, 0);
+
+        const stockPlan = stockPlans.find(sp => sp._id === stockPlanId);
+        console.log('stockPlan', stockPlan);
+        const name = stockPlan ? `${stockPlan.plan_name}` : 'Unknown Stock Plan';
+
+        return {
+            name,
+            fullyDilutedShares,
+            fullyDilutedPercentage: (fullyDilutedShares / totalOutstandingShares * 100).toFixed(2)
+        };
+    });
+}
+
+const groupIssuancesByStockClass = (issuances, stockClassIdPath) => {
+    return issuances.reduce((acc, issuance) => {
+        const stockClassId = get(issuance, stockClassIdPath) || 'general';
+        if (!acc[stockClassId]) {
+            acc[stockClassId] = [];
+        }
+        acc[stockClassId].push(issuance);
+        return acc;
+    }, {});
+}
+
+const groupIssuancesByStockPlan = (issuances) => {
+    return issuances.reduce((acc, issuance) => {
+        const stockPlanId = issuance.stock_plan_id;
+        if (!acc[stockPlanId]) {
+            acc[stockPlanId] = [];
+        }
+        acc[stockPlanId].push(issuance);
+        return acc;
+    }, {});
+}
 
 // Note: warrants only have fully diluted shares and fds %
-const calculateWarrantAndNonPlanAwardSummary = (warrantIssuances, equityCompensationIssuancesWithoutStockPlan) => {
-
+const calculateWarrantAndNonPlanAwardSummary = (stockClasses, warrantIssuances, equityCompensationIssuancesWithoutStockPlan, totalOutstandingShares) => {
     console.log('warrantIssuances', warrantIssuances);
     console.log('equityCompensationIssuancesWithoutStockPlan', equityCompensationIssuancesWithoutStockPlan);
 
-    // // find the stockClassId that each warrant converts to
-    // const warrantStockClassIds = warrantIssuances.map(warrant => warrant.convert_to_stock_class_id);
+    /*
+        {
+            stockClassId1: [warrant1, warrant2, ...],
+            general: [warrant3, warrant4, ...]
+        }
+    */
+    const warrantsByStockClass = groupIssuancesByStockClass(warrantIssuances, 'exercise_triggers.0.conversion_right.converts_to_stock_class_id');
+    const equityCompensationByStockClass = groupIssuancesByStockClass(equityCompensationIssuancesWithoutStockPlan, 'stock_class_id');
 
-    // // const rowsWarrant = warrantIssuances.map(warrant => {
-    // //     return {
-    // //         name: "some name to fill",
-    // //         fullyDilutedShares: ,
-    // //         fullyDilutedPercentage: "some number to fill"
-    // //     }
-    // // })
+    console.log('warrantsByStockClass', warrantsByStockClass);
+    console.log('equityCompensationByStockClass', equityCompensationByStockClass);
 
-    // const rowsEquityCompensation = equityCompensationIssuancesWithoutStockPlan.map(equityCompensation => {
-    //     return {
-    //         name: "some name to fill",
-    //         fullyDilutedShares: "some number to fill",
-    //         fullyDilutedPercentage: "some number to fill"
-    //     }
-    // })
+    const warrantRows = createWarrantAndNonPlanAwardsRow(warrantsByStockClass, stockClasses, totalOutstandingShares, 'Warrants', true);
+    const equityCompensationRows = createWarrantAndNonPlanAwardsRow(equityCompensationByStockClass, stockClasses, totalOutstandingShares, 'Non-Plan Awards');
+
+    console.log('warrantRows', warrantRows);
+    console.log('equityCompensationRows', equityCompensationRows);
+
+    return {
+        rows: [...warrantRows, ...equityCompensationRows]
+    }
+};
 
 
+const calculateStockPlanSummary = (stockPlans, equityCompensationIssuances, totalOutstandingShares) => {
+    // Filter equity compensation issuances with stock plans
+    const equityCompensationWithPlan = equityCompensationIssuances.filter(issuance => issuance.stock_plan_id);
+
+    // Group issuances by stock plan
+    const equityCompensationByStockPlan = groupIssuancesByStockPlan(equityCompensationWithPlan);
+
+    console.log('equityCompensationByStockPlan', equityCompensationByStockPlan);
+
+    const rows = createEquityCompensationWithPlanSummaryRows(equityCompensationByStockPlan, stockPlans, totalOutstandingShares);
+
+    return {
+        totalSharesAuthorized: stockPlans.reduce((sum, plan) => sum + Number(plan.initial_shares_reserved), 0),
+        rows
+    };
 }
-
 
 const calculateCaptableStats = async (issuerId) => {
     // First Section: Stock Classes
     const stockClasses = await getAllStockClasses(issuerId);
     const stockIssuances = await getStockIssuances(issuerId);
     const warrantIssuances = await getAllWarrants(issuerId);
+    const stockPlans = await getAllStockPlans(issuerId);
     const equityCompensationIssuances = await getAllEquityCompensationIssuances(issuerId);
-    const equityCompensationIssuancesWithoutStockPlan = equityCompensationIssuances.filter(issuance => issuance.stock_plan_id === null);
+    console.log('equityCompensationIssuances', equityCompensationIssuances);
+    const equityCompensationIssuancesStockPlan = equityCompensationIssuances.filter(issuance => issuance.stock_plan_id && issuance.stock_plan_id !== '');
+    const equityCompensationIssuancesWithoutStockPlan = equityCompensationIssuances.filter(issuance => !get(issuance, 'stock_plan_id', null));
 
     // Creates a map of stockClassId to the total number of shares issued
     const outstandingSharesByStockClass = stockIssuances.reduce((acc, issuance) => {
@@ -166,7 +259,8 @@ const calculateCaptableStats = async (issuerId) => {
             common: calculateStockClassSummary(commonStockClasses, stockIssuances, totalOutstandingShares, totalVotingPower),
             preferred: calculateStockClassSummary(preferredStockClasses, stockIssuances, totalOutstandingShares, totalVotingPower, StockIssuanceTypes.FOUNDERS_STOCK),
             founderPreferred: calculateFounderPreferredSummary(preferredStockClasses, stockIssuances, totalOutstandingShares, totalVotingPower),
-            warrantsAndNonPlanAwards: calculateWarrantAndNonPlanAwardSummary(warrantIssuances, equityCompensationIssuancesWithoutStockPlan)
+            warrantsAndNonPlanAwards: calculateWarrantAndNonPlanAwardSummary(stockClasses, warrantIssuances, equityCompensationIssuancesWithoutStockPlan, totalOutstandingShares),
+            stockPlans: calculateStockPlanSummary(stockPlans, equityCompensationIssuancesStockPlan, totalOutstandingShares)
         }
     }
 
@@ -174,6 +268,3 @@ const calculateCaptableStats = async (issuerId) => {
 };
 
 export default calculateCaptableStats;
-
-
-// calculate the totalSharesAuthorized in the frontend.
