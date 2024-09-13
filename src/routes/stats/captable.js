e5bd7d22cfe9cf58020e35f5dad5722890087953
Adam Momen <adammuman81@gmail.com>
2024-09-13 22:05:06 +0800

    fix stockPlan appending data on empty state

--- src/routes/stats/captable.js
+++ src/routes/stats/captable.js
@@ -231,11 +231,13 @@ const calculateStockPlanSummary = (stockPlans, equityCompensationIssuances, tota
     // Add the 'Available for Grants' row
     const finalRows = [
         ...rows,
-        {
+    ];
+    if(availableForGrants > 0) {
+        finalRows.push({
             name: 'Available for Grants',
             fullyDilutedShares: availableForGrants
-        }
-    ];
+        })
+    }
 
     return {
         totalSharesAuthorized,
