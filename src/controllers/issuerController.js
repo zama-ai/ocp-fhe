3965e8c60f4cf9c90e6aecfb3681e877b1b83038
Adam Momen <adammuman81@gmail.com>
2024-10-25 17:27:04 +0800

    Add linting

--- src/controllers/issuerController.js
+++ src/controllers/issuerController.js
@@ -1,14 +1,9 @@
 import { toScaledBigNumber } from "../utils/convertToFixedPointDecimals.js";
-
-export const convertAndAdjustIssuerAuthorizedSharesOnChain = async (
-    contract,
-    { new_shares_authorized, board_approval_date = "", stockholder_approval_date = "", comments }
-) => {
-    const tx = await contract.adjustIssuerAuthorizedShares(
-        toScaledBigNumber(new_shares_authorized),
-        comments,
-        board_approval_date,
-        stockholder_approval_date
-    );
-    await tx.wait();
-};
+import { withChainErrorHandler } from "./helper.js";
+export const convertAndAdjustIssuerAuthorizedSharesOnChain = withChainErrorHandler(
+    async (contract, { new_shares_authorized, board_approval_date = "", stockholder_approval_date = "", comments = [] }) => {
+        const scaledSharesAuthorized = toScaledBigNumber(new_shares_authorized);
+        const tx = await contract.adjustIssuerAuthorizedShares(scaledSharesAuthorized, comments, board_approval_date, stockholder_approval_date);
+        await tx.wait();
+    }
+);
