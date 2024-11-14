0a19aeaec5ad9a89dfc8ec2ba821643323ac680a
Victor Mimo <victormimoc@gmail.com>
2024-11-14 10:35:51 -0500

    updated imports and linted

--- chain/src/DiamondCapTable.sol
+++ chain/src/DiamondCapTable.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {LibDiamond} from "../lib/diamond-3-hardhat/contracts/libraries/LibDiamond.sol";
-import {IDiamondCut} from "../lib/diamond-3-hardhat/contracts/interfaces/IDiamondCut.sol";
-import {Diamond} from "../lib/diamond-3-hardhat/contracts/Diamond.sol";
+import { LibDiamond } from "diamond-3-hardhat/contracts/libraries/LibDiamond.sol";
+import { IDiamondCut } from "diamond-3-hardhat/contracts/interfaces/IDiamondCut.sol";
+import { Diamond } from "diamond-3-hardhat/contracts/Diamond.sol";
 
 contract DiamondCapTable is Diamond {
     constructor(address _contractOwner, address _diamondCutFacet) Diamond(_contractOwner, _diamondCutFacet) {
