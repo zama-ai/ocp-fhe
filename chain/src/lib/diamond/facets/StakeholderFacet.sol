1343c67970f7e358d771d8070b45474365639e15
Victor Mimo <victormimoc@gmail.com>
2024-11-21 11:41:38 -0500

    adding NFT, test is failing on one condition

--- chain/src/lib/diamond/facets/StakeholderFacet.sol
+++ chain/src/lib/diamond/facets/StakeholderFacet.sol
@@ -2,10 +2,14 @@
 pragma solidity ^0.8.0;
 
 import { StorageLib, Storage } from "../Storage.sol";
+import { StockActivePosition, WarrantActivePosition, ConvertibleActivePosition, EquityCompensationActivePosition, StakeholderPositions } from "../Structs.sol";
 
 contract StakeholderFacet {
     event StakeholderCreated(bytes16 indexed id);
+    event StakeholderAddressLinked(bytes16 indexed stakeholder_id, address indexed wallet_address);
+
     error StakeholderAlreadyExists(bytes16 stakeholder_id);
+    error AddressAlreadyLinked(address wallet_address);
 
     function createStakeholder(bytes16 _id) external {
         Storage storage ds = StorageLib.get();
@@ -18,5 +22,55 @@ contract StakeholderFacet {
         ds.stakeholderIndex[_id] = ds.stakeholders.length;
 
         emit StakeholderCreated(_id);
+    }
+
+    function linkStakeholderAddress(bytes16 stakeholder_id, address wallet_address) external {
+        Storage storage ds = StorageLib.get();
+
+        // Check if address is already linked
+        if (ds.addressToStakeholderId[wallet_address] != bytes16(0)) {
+            revert AddressAlreadyLinked(wallet_address);
+        }
+
+        // Link the address to stakeholder ID
+        ds.addressToStakeholderId[wallet_address] = stakeholder_id;
+
+        emit StakeholderAddressLinked(stakeholder_id, wallet_address);
+    }
+
+    function getStakeholderPositions(bytes16 stakeholder_id) external view returns (StakeholderPositions memory) {
+        Storage storage ds = StorageLib.get();
+
+        StakeholderPositions memory positions;
+
+        // Populate stocks
+        bytes16[] storage stockSecurities = ds.stockActivePositions.stakeholderToSecurities[stakeholder_id];
+        positions.stocks = new StockActivePosition[](stockSecurities.length);
+        for (uint i = 0; i < stockSecurities.length; i++) {
+            positions.stocks[i] = ds.stockActivePositions.securities[stockSecurities[i]];
+        }
+
+        // Populate warrants
+        bytes16[] storage warrantSecurities = ds.warrantActivePositions.stakeholderToSecurities[stakeholder_id];
+        positions.warrants = new WarrantActivePosition[](warrantSecurities.length);
+        for (uint i = 0; i < warrantSecurities.length; i++) {
+            positions.warrants[i] = ds.warrantActivePositions.securities[warrantSecurities[i]];
+        }
+
+        // Populate convertibles
+        bytes16[] storage convertibleSecurities = ds.convertibleActivePositions.stakeholderToSecurities[stakeholder_id];
+        positions.convertibles = new ConvertibleActivePosition[](convertibleSecurities.length);
+        for (uint i = 0; i < convertibleSecurities.length; i++) {
+            positions.convertibles[i] = ds.convertibleActivePositions.securities[convertibleSecurities[i]];
+        }
+
+        // Populate equity compensations
+        bytes16[] storage equityCompSecurities = ds.equityCompensationActivePositions.stakeholderToSecurities[stakeholder_id];
+        positions.equityCompensations = new EquityCompensationActivePosition[](equityCompSecurities.length);
+        for (uint i = 0; i < equityCompSecurities.length; i++) {
+            positions.equityCompensations[i] = ds.equityCompensationActivePositions.securities[equityCompSecurities[i]];
+        }
+
+        return positions;
     }
 }
---
+++ chain/test/diamond/DiamondStakeholderNFT.t.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./DiamondTestBase.sol";
+import { StorageLib } from "@diamond/Storage.sol";
+import { TxHelper, TxType } from "@diamond/DiamondTxHelper.sol";
+import { ValidationLib } from "@diamond/libraries/ValidationLib.sol";
+import { StakeholderPositions } from "@diamond/Structs.sol";
+import { StakeholderNFTFacet } from "@diamond/facets/StakeholderNFTFacet.sol";
+
+contract DiamondStakeholderNFTTest is DiamondTestBase {
+    bytes16 stakeholderId;
+    address stakeholderWallet;
+
+    function setUp() public override {
+        super.setUp();
+        stakeholderId = createStakeholder();
+        stakeholderWallet = address(0xBEEF);
+
+        // Issue some positions to the stakeholder
+        bytes16 stockClassId = createStockClass();
+        bytes16 stockSecurityId = 0xd3373e0a4dd940000000000000000001;
+        StockFacet(address(diamond)).issueStock(stockClassId, 1e18, 1000, stakeholderId, stockSecurityId);
+    }
+
+    function testLinkStakeholderAddress() public {
+        // Link the address
+        StakeholderFacet(address(diamond)).linkStakeholderAddress(stakeholderId, stakeholderWallet);
+
+        // Verify the link was created
+        bytes16 storedId = StorageLib.get().addressToStakeholderId[stakeholderWallet];
+        assertEq(storedId, stakeholderId, "Stakeholder ID should be linked to wallet");
+    }
+
+    function testMintNFT() public {
+        // Link address first
+        StakeholderFacet(address(diamond)).linkStakeholderAddress(stakeholderId, stakeholderWallet);
+
+        // Mint NFT
+        vm.prank(stakeholderWallet);
+        StakeholderNFTFacet(address(diamond)).mint();
+    }
+
+    function testFailMintWithoutLink() public {
+        vm.prank(stakeholderWallet);
+        StakeholderNFTFacet(address(diamond)).mint();
+    }
+
+    function testFailDoubleMint() public {
+        // Link address first
+        StakeholderFacet(address(diamond)).linkStakeholderAddress(stakeholderId, stakeholderWallet);
+
+        // First mint
+        vm.prank(stakeholderWallet);
+        StakeholderNFTFacet(address(diamond)).mint();
+
+        // Try to mint again - should fail
+        vm.prank(stakeholderWallet);
+        StakeholderNFTFacet(address(diamond)).mint();
+    }
+}
