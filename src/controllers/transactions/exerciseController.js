16bf7b866696de7770464a693d9e3800debdc129
Adam Momen <adammuman81@gmail.com>
2024-12-13 18:08:02 +0800

    chore: clean up

--- src/controllers/transactions/exerciseController.js
+++ src/controllers/transactions/exerciseController.js
@@ -1,21 +1,15 @@
 import { convertUUIDToBytes16 } from "../../utils/convertUUID.js";
 import { toScaledBigNumber } from "../../utils/convertToFixedPointDecimals.js";
 
-export const convertAndCreateEquityCompensationExerciseOnchain = async (
-    contract,
-    { equity_comp_security_id, resulting_stock_security_id, quantity }
-) => {
-    const equityCompSecurityIdBytes16 = convertUUIDToBytes16(equity_comp_security_id);
-    const resultingStockSecurityIdBytes16 = convertUUIDToBytes16(resulting_stock_security_id);
+export const convertAndCreateEquityCompensationExerciseOnchain = async (contract, { security_id, resulting_security_ids, quantity }) => {
+    const equityCompSecurityIdBytes16 = convertUUIDToBytes16(security_id);
+    const resultingStockSecurityIdBytes16 = convertUUIDToBytes16(resulting_security_ids[0]);
     const quantityScaled = toScaledBigNumber(quantity);
 
     const tx = await contract.exerciseEquityCompensation(equityCompSecurityIdBytes16, resultingStockSecurityIdBytes16, quantityScaled);
     await tx.wait();
     console.log("Transaction hash:", tx.hash);
 
-    console.log("✅ | Exercised equity compensation onchain, unconfirmed: ", {
-        equity_comp_security_id,
-        resulting_stock_security_id,
-        quantity,
-    });
+    console.log("[PENDING] Exercised equity compensation onchain", tx.hash);
+    return tx.hash;
 };
