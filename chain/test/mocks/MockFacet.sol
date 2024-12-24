4146feeeb3d59aed3084b91c73a88891c40da56a
Adam Momen <adammuman81@gmail.com>
2024-12-24 18:38:31 +0800

    Update MockFacet

--- chain/test/mocks/MockFacet.sol
+++ chain/test/mocks/MockFacet.sol
@@ -4,16 +4,15 @@ pragma solidity ^0.8.20;
 contract MockFacet {
     uint256 private value;
 
-    function setValue(uint256 _value) external {
-        value = _value;
-    }
-
-    function getValue() external view returns (uint256) {
-        return value;
-    }
-
-    // New function for upgrade testing
     function getValuePlusOne() external view returns (uint256) {
         return value + 1;
+    }
+}
+
+contract MockFacetV2 {
+    uint256 private value;
+
+    function getValuePlusTwo() external view returns (uint256) {
+        return value + 2;
     }
 }
