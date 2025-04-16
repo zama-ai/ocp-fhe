74d7618df7d3a06cee2a38d8cd94888c9b9a27a8
Adam Momen <adammuman81@gmail.com>
2025-04-16 19:22:00 +0800

    Update Factory object: - Update Factory.js with latest changes

--- src/db/objects/Factory.js
+++ src/db/objects/Factory.js
@@ -1,13 +1,31 @@
 import mongoose from "mongoose";
 import { v4 as uuid } from "uuid";
 
+export const FACTORY_VERSION = {
+    DIAMOND: "DIAMOND",
+    LEGACY: "LEGACY",
+};
+
 const FactorySchema = new mongoose.Schema(
     {
         _id: { type: String, default: () => uuid() },
         object_type: { type: String, default: "FACTORY" },
-        implementation_address: { type: String, required: true },
-        factory_address: { type: String, required: true },
-        chain_id: { type: Number, required: true },
+        implementation_address: String,
+        factory_address: String,
+        chain_id: {
+            type: String,
+            required: true,
+            enum: [
+                "8453", // Base mainnet
+                "84532", // Base Sepolia
+                "31337", // anvil
+            ],
+        },
+        version: {
+            type: String,
+            required: true,
+            enum: Object.values(FACTORY_VERSION),
+        },
     },
     { timestamps: true }
 );
